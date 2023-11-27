4ac605d8c8b2abcd6d4086b3eb276192f5a74b8b
joelvappiani <joel.vappiani@gmail.com>
2023-11-27 14:49:04 +0100

    working with socket and client

--- src/app.js
+++ src/app.js
@@ -3,37 +3,48 @@ import path from 'path';
 import { LlamaModel, LlamaContext, LlamaChatSession } from 'node-llama-cpp';
 import express from 'express';
 const app = express();
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const model = new LlamaModel({
     modelPath: path.join(__dirname, 'models', 'llama-2-7b-chat.Q4_K_M.gguf'),
 });
+
+app.use(cors());
 const context = new LlamaContext({ model });
 const session = new LlamaChatSession({ context });
-// stream function
-// async () => {
-//     const q1 = 'Tell me how to make a pizza?';
-//     console.log('User: ' + q1);
-//     process.stdout.write('AI: ');
-//     const a1 = await session.prompt(q1, {
-//         onToken(chunk) {
-//             process.stdout.write(context.decode(chunk));
-//         },
-//     });
-// };
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.post('/chat', async (req, res) => {
-    const prompt = `User: ${req.body.prompt}`;
-    //const response = await session.prompt(prompt);
-    process.stdout.write('AI: ');
-    const response = await session.prompt(prompt, {
-        onToken(chunk) {
-            process.stdout.write(context.decode(chunk));
-        },
+io.on('connection', (socket) => {
+    socket.on('prompt', async (data) => {
+        const prompt = `User: ${data}`;
+        console.log('AI generating');
+        let message = '';
+        await session.prompt(prompt, {
+            onToken(chunk) {
+                message += context.decode(chunk);
+                socket.emit('response', message);
+                //process.stdout.write(context.decode(chunk));
+            },
+        });
     });
-    res.json({ response });
+
+    let chunk = '';
+    process.stdout.on('data', (data) => {
+        chunk += data;
+        socket.emit('response', chunk);
+    });
 });
-app.listen(3000, () => {
+
+httpServer.listen(3000, () => {
     console.log('Server connected');
 });
